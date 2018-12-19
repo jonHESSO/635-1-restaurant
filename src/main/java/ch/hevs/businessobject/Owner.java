fcf500ed3ac54808061e2a279d03d7a6871e37b4
jonHESSO <jonathan.schnyder@students.hevs.ch>
2018-12-19 09:04:29 +0100

    Added restaurant and owner entities

---
+++ src/main/java/ch/hevs/businessobject/Owner.java
@@ -0,0 +1,64 @@
+package ch.hevs.businessobject;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Owner {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;	
+	private String name ;
+	private String email;	
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)  //bi-directional 1-n mapping
+	private Set<Restaurant> restaurants;
+	
+	public Owner() {
+	}
+
+	public Owner(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Restaurant> getRestaurants() {
+		return restaurants;
+	}
+
+	public void setRestaurants(Set<Restaurant> restaurants) {
+		this.restaurants = restaurants;
+	}
+	
+	
+}
---
+++ src/main/java/ch/hevs/businessobject/Restaurant.java
@@ -0,0 +1,96 @@
+package ch.hevs.businessobject;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Restaurant {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id ;	
+	private String name;
+	private String adress;
+	private String city ;
+	@OneToMany  //uni-directional 1-n mapping
+	private Set<Rating> ratings ;
+	@OneToOne  //uni-direction 0-1 mapping
+	@JoinColumn(name = "FK_MENU", nullable = true)
+	private Menu menu ;	
+	@ManyToOne //bi-directional 1-n mapping
+	@JoinColumn(name = "FK_OWNER", nullable = false)
+	private Owner owner ;
+	
+	public Restaurant() {
+	}
+
+	public Restaurant(String name, String adress, String city, Owner owner) {
+		this.name = name;
+		this.adress = adress;
+		this.city = city;
+		this.owner = owner;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Set<Rating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(Set<Rating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public Menu getMenu() {
+		return menu;
+	}
+
+	public void setMenu(Menu menu) {
+		this.menu = menu;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+	
+}
