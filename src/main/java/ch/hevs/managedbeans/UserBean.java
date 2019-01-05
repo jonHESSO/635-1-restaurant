971974fa0d1f15e8e46ef54f3e5a4667f564327f
EndritHaziri <endrit_haziri@hotmail.com>
2019-01-05 16:50:35 +0100

    Start of "UserBean" and modification of "pom.xml"

--- src/main/java/ch/hevs/managedbeans/UserBean.java
+++ src/main/java/ch/hevs/managedbeans/UserBean.java
@@ -1,5 +1,39 @@
 package ch.hevs.managedbeans;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.event.ValueChangeEvent;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ch.hevs.businessobject.Owner;
+import ch.hevs.businessobject.Restaurant;
+import ch.hevs.starservice.Star;
+
 public class UserBean {
+	
+	
+	private List<Restaurant> restaurants;
+	private Restaurant restaurant;
+	private Owner owner;
+	private String comment;
+	private Star star;
+	
+	@PostConstruct
+	public void initialize() throws NamingException {
+		InitialContext ctx = new InitialContext();
+		star = (Star) ctx.lookup("java:global/SCHNYDER-HAZIRI-WEB-EJB-0.0.1-SNAPSHOT/StarBean!ch.hevs.starservice.Star");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
